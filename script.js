5b58960ea5a0e974c77619b51585cd19b685166d
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-14 19:42:37 -0800

    add event handlers to change brush color

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 (function() {
   'use strict';
 
+  // Create grid
   const grid = document.querySelector('#grid');
 
   let pixel;
   for (let i = 0; i < 1776; i++) {
     pixel = document.createElement('div');
-    pixel.classList.add('pixel')
+    pixel.classList.add('pixel');
     grid.appendChild(pixel);
   }
 
+  // Create color pallete
   const pallete = document.querySelector('#pallete');
 
   let colors = [
@@ -26,14 +28,29 @@
 
   let palleteColor;
   for (const color of colors) {
-    palleteColor = document.createElement('div')
+    palleteColor = document.createElement('div');
     palleteColor.classList.add('palleteColor', color);
     pallete.appendChild(palleteColor);
   }
 
+  // Event handlers
   let brushColor = 'black';
 
   grid.addEventListener('click', (e) => {
-    e.target.classList.add(brushColor)
+    if (e.target.id === 'grid') {
+      return;
+    }
+    console.log(e.target);
+    e.target.classList.add(brushColor);
   });
+
+  pallete.addEventListener('click', (e) => {
+    if (e.target.id === 'pallete') {
+      return;
+    }
+    console.log(e.target);
+    brushColor = e.target.classList.item(1);
+  })
+
+
 })();
