4fbfe273432123708bbe4c650380a4b018600f6d
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-14 19:33:40 -0800

    add color pallete

--- script.js
+++ script.js
@@ -2,13 +2,38 @@
   'use strict';
 
   const grid = document.querySelector('#grid');
+
   let pixel;
   for (let i = 0; i < 1776; i++) {
     pixel = document.createElement('div');
     pixel.classList.add('pixel')
     grid.appendChild(pixel);
   }
+
+  const pallete = document.querySelector('#pallete');
+
+  let colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'black',
+    'grey',
+    'white'
+  ];
+
+  let palleteColor;
+  for (const color of colors) {
+    palleteColor = document.createElement('div')
+    palleteColor.classList.add('palleteColor', color);
+    pallete.appendChild(palleteColor);
+  }
+
+  let brushColor = 'black';
+
   grid.addEventListener('click', (e) => {
-    e.target.classList.add('red')
+    e.target.classList.add(brushColor)
   });
 })();
